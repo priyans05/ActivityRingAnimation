ed7359f5f46944be850a48ca8b18a865e3a895db
priyans <priyans05@gmail.com>
2020-01-28 11:01:50 +1100

    Initial Commit

---
+++ ActivityRingAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActivityRingAnimation
+//
+//  Created by PRIYANS on 28/1/20.
+//  Copyright © 2020 PRIYANS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
